f07024c93854f8a1bf7e37c91c801ae693a3993a
Vladimir <fominsky_v@live.com>
2023-04-19 21:42:32 +0500

    work_with_Hero_Weapon

--- game/core/src/ru/gb/game/Hero.java
+++ game/core/src/ru/gb/game/Hero.java
@@ -3,10 +3,14 @@ package ru.gb.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.StringBuilder;
 
 
 public class Hero {
@@ -16,14 +20,53 @@ public class Hero {
     private Vector2 velocity;
     private float angle;
     private float enginePower;
+    public float fireTimer;
+    private int score; // Сколько баллов набрали
+    private int scoreView; // Сколько баллов отображаем
+    private int hp;
+    private int hpMax;
+    private Circle hitArea;
+    private StringBuilder sbScore;
+    private StringBuilder sbHP;
+    private StringBuilder sbGameOver;
+    private StringBuilder sbAmmo;
+    private StringBuilder sbMoney;
+    private Weapon weapon;
     private ParticleController pc;
     private int halfX;
     private int halfY;
+    private int money;
     private final float BASE_SIZE = 64;
     private final float BASE_RADIUS = BASE_SIZE / 2;
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public Circle getHitArea() {
+        return hitArea;
+    }
+
+    public int getHpMax() {
+        return hpMax;
+    }
+
+    public void addScore(int amount) {
+        this.score += amount;
+    }
+
     public float getAngle() {
         return angle;
     }
+
     public Vector2 getPosition() {
         return position;
     }
@@ -39,15 +82,71 @@ public class Hero {
         this.angle = 0.0f;
         this.enginePower = 500.0f;
         this.pc = pc;
+        this.hitArea = new Circle();
+        this.hitArea.setPosition(position);
+        this.hitArea.setRadius(BASE_RADIUS);
+        this.hpMax = 100;
+        this.hp = hpMax;
+        this.sbScore = new StringBuilder();
+        this.sbHP = new StringBuilder();
+        this.sbGameOver = new StringBuilder();
+        this.sbAmmo = new StringBuilder();
+        this.sbMoney = new StringBuilder();
+        createWeapons();
     }
 
     public ParticleController getPc() {
         return pc;
     }
 
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public boolean comeToHero(BonusController.Bonus bb) {
+        if(this.position.dst(bb.getPosition()) < 60) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean takeDamage(int amout) {
+        hp -= amout;
+        if(hp <= 0) {
+            // уничтожение корабля
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public void render(SpriteBatch batch) {
         batch.draw(texture, position.x - 32, position.y - 32, 32, 32, 64, 64, 1, 1, angle);
         pc.render(batch);
+    }
+
+    public void renderGUI(SpriteBatch batch, BitmapFont font) {
+        sbScore.clear();
+        sbHP.clear();
+        sbGameOver.clear();
+        sbAmmo.clear();
+        sbMoney.clear();
+        sbScore.append("SCORE: ").append(scoreView); // Получаем счет
+        font.draw(batch, sbScore, 10, 180); // Выводим счет на экран
+        sbHP.append("HP: ").append(hp).append(" / ").append(hpMax); // Получаем hp корабля
+        font.draw(batch, sbHP, 10, 160); // Выводим hp корабля на экран
+        sbAmmo.append("Ammo: ").append(weapon.getCurBullets()).append(" / ").append(weapon.getMaxBullets()); // Получаем количество патронов
+        font.draw(batch, sbAmmo, 10, 140); // Выводим количество патронов на экран
+        sbMoney.append("Money: ").append(money); // Получаем количество патронов
+        font.draw(batch, sbMoney, 10,  120); // Выводим количество патронов на экран
+
+        halfX = Gdx.graphics.getWidth() / 2;
+        halfY = Gdx.graphics.getHeight() / 2;
+
+        if(hp <= 0) {
+            sbGameOver.append("Game Over");
+            font.draw(batch, sbGameOver, halfX, halfY); //
+        }
     }
 
     public void update(float dt) {
@@ -84,6 +183,15 @@ public class Hero {
         }
     }
 
+    public void updateScore(float dt) {
+        if(scoreView < score) {
+            scoreView += 1000 * dt;
+            if(scoreView > score) {
+                scoreView = score;
+            }
+        }
+    }
+
     public void push(Vector2 pushVector) {
         velocity.set(pushVector);
     }
@@ -108,7 +216,14 @@ public class Hero {
     }
 
     public void controlShip(float dt) {
+        fireTimer += dt;
+        if(Gdx.input.isKeyPressed(Input.Keys.P)) {
 
+            if(fireTimer > 0.2f) {
+                fireTimer = 0.0f;
+                weapon.fire();
+            }
+        }
         if(Gdx.input.isKeyPressed(Input.Keys.A)) {
             angle += 180.0f * dt;
         }
@@ -131,5 +246,26 @@ public class Hero {
             stopKoef = 0;
         }
         velocity.scl(stopKoef); // scl() - Умножение вектора на скаляр
+    }
+
+    public void useBonus(int size, int type) {
+        if(type == 0 || type == 1 ) {
+            money += size;
+        }
+        if(type == 2 || type == 3 ) {
+            hp += size;
+        }
+        if(type == 4 || type == 5 ) {
+            weapon.setCurBullets(size);
+        }
+    }
+
+    private void createWeapons() {
+        weapon = new Weapon(
+                this, "Energy", 0.2f, 4,
+                        2000, 1000,
+                        new Vector3[]{
+                                new Vector3(28, 0, 0)
+                        });
     }
 }
--- game/core/src/ru/gb/game/Weapon.java
+++ game/core/src/ru/gb/game/Weapon.java
@@ -1,10 +1,23 @@
 package ru.gb.game;
 
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+
 public class Weapon {
-
+    private BulletController bc;
+    private Hero hero;
+    private String title;
+    private float firePeriod;
+    private int damage;
+    private float bulletSpeed;
     private int curBullets;
-
     private int maxBullets;
+    private Vector3[] slots;
+    private Sound shootSound;
+    public float getFirePeriod() {
+        return firePeriod;
+    }
 
     public int getCurBullets() {
         return curBullets;
@@ -14,6 +27,43 @@ public class Weapon {
         return maxBullets;
     }
 
+    public void setCurBullets(int bullet) {
+        curBullets += bullet;
+    }
+
+    public Weapon(Hero hero, String title,
+                  float firePeriod, int damage, float bulletSpeed,
+                  int maxBullets, Vector3[] slots) {
+        this.hero = hero;
+        this.title = title;
+        this.firePeriod = firePeriod;
+        this.damage = damage;
+        this.bulletSpeed = bulletSpeed;
+        this.maxBullets = maxBullets;
+        this.curBullets = maxBullets;
+        this.slots = slots;
+        this.bc = new BulletController(hero.getPc());
+    }
+
+    public BulletController getBc() {
+        return bc;
+    }
+
+
     public void fire() {
+        if (curBullets > 0) {
+            curBullets--;
+            for (int i = 0; i < slots.length; i++) {
+                float x, y, vx, vy;
+
+                x = hero.getPosition().x + MathUtils.cosDeg(hero.getAngle() + slots[i].y) * slots[i].x;//25; // Место вылета пули x
+                y = hero.getPosition().y + MathUtils.sinDeg(hero.getAngle() + slots[i].y) * slots[i].x;//25;
+
+                vx = hero.getVelocity().x + bulletSpeed * MathUtils.cosDeg(hero.getAngle() + slots[i].z);
+                vy = hero.getVelocity().y + bulletSpeed * MathUtils.sinDeg(hero.getAngle() + slots[i].z);
+
+                bc.setup(x, y, vx, vy);
+            }
+        }
     }
 }
